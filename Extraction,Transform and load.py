dda58d5928735069020c74c86a79c63a8d5f4eb9
Feliciarethabile <158004313+Feliciarethabile@users.noreply.github.com>
2024-02-03 20:07:19 +0200

    Add files via upload

---
+++ Extraction,Transform and load.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+
+df = pd.read_csv("data_02/country_data_index.csv")
+df = pd.read_csv("https://archive.cs.uci.edu/ml/machine-learning-databases/iris/iris.data")
